04b0176334862ca248a0cd40cd7c217dbaa36340
Magnolia Benjamin <magnoliaebv16@gmail.com>
2020-10-10 11:49:46 -0400

    conf api key

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,6 @@
 export const config = {
     URL_BASE: 'https://gateway.marvel.com:443/v1/public',
-    API_KEY: '99982b4ebc3a0ca86cb9b5d1e051f3bc',
-    // API_KEY: '1f403d737318aac92cfaaecad9bac524',
+    API_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
 }
 
 
-// carlos@extrasistemas.com
-// :r.-a?-8?c26U#N
